d9ae3ab431fe70feb82daa4538e09ebab453c232
Gerardo Gutierrez <gerardog3@cpp.edu>
2024-09-10 00:22:04 -0700

    upload simulation project

---
+++ src/powerGridSimulation/Location.java
@@ -0,0 +1,38 @@
+package powerGridSimulation;
+
+public class Location {
+
+	public String locationId;
+	public int numApp;
+	
+	public int affectedCounter;
+	
+	public Location(String locID, int numApp, int affectedCounter) {
+		this.locationId = locID;
+		this.numApp = numApp;
+		this.affectedCounter = affectedCounter;
+	}
+	
+	
+	
+	public void incrementAffectedCounter() {
+		this.affectedCounter ++;
+	}
+	
+	
+	public int getAffectedCounter() {
+		return this.affectedCounter;
+	}
+	
+	
+	public String getlocID() {
+		return this.locationId;
+	}
+	
+	public int getNumApp() {
+		return this.numApp;
+	}
+	
+	
+	
+}
