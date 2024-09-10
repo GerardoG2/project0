bb26b1541a885695f2da1abc6aec0239307279bd
Gerardo Gutierrez <gerardog3@cpp.edu>
2024-09-10 01:22:59 -0700

    !

--- src/powerGridSimulation/Main.java
+++ src/powerGridSimulation/Main.java
@@ -18,7 +18,7 @@ public class Main {
 	
     public static void main(String[] args) throws IOException {
     	
-    	//System.out.print("Hello World!");
+    	//System.out.print("Hello World!!");
         ArrayList<Appliance> applianceList = new ArrayList<>();
         ArrayList<String> applianceID = new ArrayList<>();
         
