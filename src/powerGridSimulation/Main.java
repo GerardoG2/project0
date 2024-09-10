e76acec37d6da64982b8e806fe8f38068aece796
Gerardo Gutierrez <gerardog3@cpp.edu>
2024-09-10 03:06:53 -0700

    shoe

--- src/powerGridSimulation/Main.java
+++ src/powerGridSimulation/Main.java
@@ -18,7 +18,7 @@ public class Main {
 	
     public static void main(String[] args) throws IOException {
     	
-    	//System.out.print("Hello World! apple");
+    	//System.out.print("Hello World! shoe");
         ArrayList<Appliance> applianceList = new ArrayList<>();
         ArrayList<String> applianceID = new ArrayList<>();
         
